69e3e592c36bfd0bf7765269585f9225e37bfa39
vamshikrishnakalleti <97330179+vamshikrishnakalleti@users.noreply.github.com>
2023-02-13 15:36:50 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-hostname=$(curl http://169.254.169.254/metadata/v1/hostname)
+hostname=$(curl http://192.168.1.47/metadata/v1/hostname)
 docker run -d -p 80:8080 --name openmct-"$hostname" mvkaran/openmct:"$hostname"
